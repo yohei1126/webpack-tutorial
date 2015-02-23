4788af14940923559dc9e1e55c33a16db464ea0a
Yohei Onishi <vivre214@gmail.com>
2015-02-23 22:06:23 +0900

    delete node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    entry: "./entry.js",
+    entry: "./entry.coffee",
     output: {
         path: __dirname,
         filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.coffee$/, loader: "coffee-loader" },
+            { test: /\.(coffee\.md|litcoffee)$/, loader: "coffee-loader?literate" }
         ]
     }
 };
